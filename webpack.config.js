5af1fcf55fcf1a2edc022fff51e2cda80d5cb0a8
Harshit Bhawsar <harshit.bhawsar@taqtics.co>
2024-08-03 08:03:16 +0000

    [WEBPACK]: Native development server added

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path')
 
 module.exports = {
+    mode: 'development',
     entry: './src/app.ts',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/dist/'
     },
     devtool: 'inline-source-map', //? prerequesities: enable sourceMap in tsconfig.
     module: {
@@ -17,5 +19,12 @@ module.exports = {
         ]
     }, resolve: {
         extensions: ['.ts', '.js']
-    }
+    },
+    devServer: {
+        static: [
+            {
+                directory: path.join(__dirname),
+            },
+        ],
+    },
 }
